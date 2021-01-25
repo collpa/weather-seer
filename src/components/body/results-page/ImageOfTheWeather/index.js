dd70f6b648d89cc8d282e6065bba99970151163d
collpa <72576026+collpa@users.noreply.github.com>
2021-01-25 16:06:05 +0100

    Added  ImageOfTheWeather

---
+++ src/components/body/results-page/ImageOfTheWeather/index.js
@@ -0,0 +1,11 @@
+import "./styles.scss";
+
+const ImageOfTheWeather = () => {
+  return (
+    <div className={"weather-image-position"}>
+      <img src={"https://via.placeholder.com/398x259"} alt={"Empty frame"} />
+    </div>
+  );
+};
+
+export default ImageOfTheWeather;
