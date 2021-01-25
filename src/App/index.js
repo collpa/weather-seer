0d85847125532294f791c4991267668970fd9c26
collpa <72576026+collpa@users.noreply.github.com>
2021-01-25 16:04:00 +0100

    Added icons for weather details

--- src/App/index.js
+++ src/App/index.js
@@ -1,15 +1,18 @@
 import React from "react";
+import "./styles.scss";
 import SocialButton from "../components/header/SocialButton";
 import Title from "../components/header/Title";
 import ExplanationText from "../components/body/home-page/ExplanationText";
 import SearchBar from "../components/body/home-page/SearchBar";
 import SubmitButton from "../components/body/home-page/SubmitButton";
+import ImageOfTheWeather from "../components/body/results-page/ImageOfTheWeather";
+import WeatherDetails from "../components/body/results-page/WeatherDeatils";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showResultWeather: false,
+      showResultWeather: true,
     };
   }
 
@@ -19,7 +22,20 @@ class App extends React.Component {
         <SocialButton />
         <Title />
         {this.state.showResultWeather ? (
-          <></>
+          <>
+            <ImageOfTheWeather />
+            <div className={"flex-icons"}>
+              <WeatherDetails />
+              <WeatherDetails />
+              <WeatherDetails />
+            </div>
+            <div className={"flex-icons"}>
+              <WeatherDetails />
+              <WeatherDetails />
+              <WeatherDetails />
+            </div>
+            <SubmitButton />
+          </>
         ) : (
           <>
             <ExplanationText />
---
+++ src/components/body/results-page/WeatherDeatils/index.js
@@ -0,0 +1,14 @@
+import "./styles.scss";
+
+const WeatherDetails = () => {
+  return (
+    <div className={"container-position"}>
+      <div className={"icon-position"}>
+        <img src={"https://via.placeholder.com/60"} alt={"Icon frame"} />
+      </div>
+      <p>{"vento"}</p>
+    </div>
+  );
+};
+
+export default WeatherDetails;
